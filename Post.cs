9a2b038385031503ff4eaba9b69ae396b36b0267
thedineshbabu <thedineshbabu@hotmail.com>
2020-06-09 14:28:58 -0500

    WIP

---
+++ Post.cs
@@ -0,0 +1,15 @@
+namespace EFCore 
+{
+    public class Post 
+    {
+        public int PostId { get; set; }
+
+        public string Title { get; set; }
+
+        public string Content { get; set; }
+
+        public int BlogId { get; set; }
+
+        public Blog Blog { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Linq;
+
+namespace EFCore
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            using (var db = new BloggingContext())
+            {   
+                // Create
+                System.Console.WriteLine("Inserting a new Blog");
+                db.Add(new Blog{ Url = "http://blogs.thedineshbabu.com/data"});
+                db.SaveChanges();
+
+                //Read
+                System.Console.WriteLine("Querying for a blog");
+                var blog = db.Blogs.OrderBy(s=>s.BlogId).First();
+
+                //Update
+                System.Console.WriteLine("Updating a Blog and adding a post");
+                blog.Url = "";
+                blog.Posts.Add(new Post()
+                {
+                    Title = "Post Updated",
+                    Content = "Sample Content EF"
+                });
+                db.SaveChanges();
+
+                //Delete
+                System.Console.WriteLine("Delete the blog");
+                db.Remove(blog);
+                db.SaveChanges();
+
+            }
+        }
+    }
+}
